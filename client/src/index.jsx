8cd273807b4f8879173442e5d112f09d7d30707f
Zedong Wang <agostowzd@gmail.com>
2021-05-13 15:18:15 -0400

    Done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,6 @@
 // TODO: Render the `App` component to the DOM
+import App from './components/App.jsx';
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<App />, document.getElementById('app'));
